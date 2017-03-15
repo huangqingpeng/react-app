b3c23fcf44445f4bb284dad4112b7335e5e13665
unknown <2692617311@qq.com>
2017-03-15 20:28:04 +0800

    hjgj

--- xiangmu/src/components/mine.js
+++ xiangmu/src/components/mine.js
@@ -7,8 +7,39 @@ import {Link} from "react-router"
 class Mine extends React.Component{
 	render(){
 		return(
-			<div>
-			    <div className="m-mine">mine我的</div>
+			<div className="mine_box">
+			    <header>
+			    	<div className="head">
+			    		<div className="icon">
+			    			<span></span>
+			    		</div>
+			    		<img src="./img/mine_touxiang.png"/><br/>
+			    		<a href="##">点击登录</a>
+			    	</div>
+			    	<div clssName="header_list">
+			    		<ul>
+			    			<li>
+			    				<p>&#xe60a;</p>
+			    				<h2>关注</h2>
+			    			</li>
+			    			<li>
+			    				<p>&#xe638;</p>
+			    				<h2>足迹</h2>
+			    			</li>
+			    			<li>
+			    				<p>&#xe68d;</p>
+			    				<h2>好友</h2>
+			    			</li>
+			    		</ul>
+			    	</div>
+			    </header>
+			    <section>
+			    	<ul>
+			    		<li>
+			    			
+			    		</li>
+			    	</ul>
+			    </section>
 			</div>
 		)
 	}
