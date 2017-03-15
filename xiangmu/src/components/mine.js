25d70053e75a2416c600c07f68fd5d4de4f6584b
unknown <2692617311@qq.com>
2017-03-15 22:29:22 +0800

    jhkjhhkjk

--- xiangmu/src/components/mine.js
+++ xiangmu/src/components/mine.js
@@ -11,23 +11,24 @@ class Mine extends React.Component{
 			    <header>
 			    	<div className="head">
 			    		<div className="icon">
-			    			<span></span>
+			    			<i className="yo-ico">&#xe635;</i>
+			    			<i className="yo-ico">&#x3466;</i>
 			    		</div>
 			    		<img src="./img/mine_touxiang.png"/><br/>
 			    		<a href="##">点击登录</a>
 			    	</div>
-			    	<div clssName="header_list">
+			    	<div className="header_list">
 			    		<ul>
 			    			<li>
-			    				<p>&#xe60a;</p>
+			    				<i className="yo-ico">&#xe69c;</i>
 			    				<h2>关注</h2>
 			    			</li>
 			    			<li>
-			    				<p>&#xe638;</p>
-			    				<h2>足迹</h2>
+			    				<i className="yo-ico">&#xe615;</i>
+			    				<h2>影迹</h2>
 			    			</li>
 			    			<li>
-			    				<p>&#xe68d;</p>
+			    				<i className="yo-ico">&#xe602;</i>
 			    				<h2>好友</h2>
 			    			</li>
 			    		</ul>
@@ -36,7 +37,38 @@ class Mine extends React.Component{
 			    <section>
 			    	<ul>
 			    		<li>
-			    			
+			    			<p><i className="yo-ico">&#xe66f;</i>客服反馈</p>
+			    			<i> > </i>
+			    		</li>
+			    		<li>
+			    			<p><i className="yo-ico">&#xe67b;</i>客服反馈</p>
+			    			<i> > </i>
+			    		</li>
+			    		<li>
+			    			<p><i className="yo-ico">&#xe630;</i>客服反馈</p>
+			    			<i> > </i>
+			    		</li>
+			    		<li>
+			    			<p><i className="yo-ico">&#xe601;</i>客服反馈</p>
+			    			<i><i className="yo-ico">&#xe688;</i>
+			    			<i className="yo-ico">&#x3434;</i>
+			    			<i className="yo-ico">&#xe616;</i> > </i>
+			    		</li>
+			    	</ul>
+			    	<ul>
+			    		<li>
+			    			<p><i className="yo-ico">&#xe610;</i>客服反馈</p>
+			    			<i> > </i>
+			    		</li>
+			    		<li>
+			    			<p><i className="yo-ico">&#xe61b;</i>客服反馈</p>
+			    			<i> > </i>
+			    		</li>
+			    	</ul>
+			    	<ul>
+			    		<li>
+			    			<p><i className="yo-ico">&#xe643;</i>客服反馈</p>
+			    			<i> > </i>
 			    		</li>
 			    	</ul>
 			    </section>
