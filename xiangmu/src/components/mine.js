07afe9b360e24f45837cb3552323ffe7bda229fe
unknown <2692617311@qq.com>
2017-03-16 08:48:18 +0800

    zmm

--- xiangmu/src/components/mine.js
+++ xiangmu/src/components/mine.js
@@ -12,6 +12,7 @@ class Mine extends React.Component{
 			    	<div className="head">
 			    		<div className="icon">
 			    			<i className="yo-ico">&#xe635;</i>
+			    			<span></span>
 			    			<i className="yo-ico">&#x3466;</i>
 			    		</div>
 			    		<img src="./img/mine_touxiang.png"/><br/>
@@ -34,44 +35,44 @@ class Mine extends React.Component{
 			    		</ul>
 			    	</div>
 			    </header>
-			    <section>
+			    <div className="main_list">
 			    	<ul>
 			    		<li>
-			    			<p><i className="yo-ico">&#xe66f;</i>客服反馈</p>
-			    			<i> > </i>
+			    			<p><i className="yo-ico">&#xe66f;</i><span>我的订单</span></p>
+			    			<i> &gt; </i>
 			    		</li>
 			    		<li>
-			    			<p><i className="yo-ico">&#xe67b;</i>客服反馈</p>
-			    			<i> > </i>
+			    			<p><i className="yo-ico">&#xe67b;</i><span>我的钱包</span></p>
+			    			<i> &gt; </i>
 			    		</li>
 			    		<li>
-			    			<p><i className="yo-ico">&#xe630;</i>客服反馈</p>
-			    			<i> > </i>
+			    			<p><i className="yo-ico">&#xe630;</i><span>账户设置</span></p>
+			    			<i> &gt; </i>
 			    		</li>
 			    		<li>
-			    			<p><i className="yo-ico">&#xe601;</i>客服反馈</p>
+			    			<p><i className="yo-ico">&#xe601;</i><span>登录方式</span></p>
 			    			<i><i className="yo-ico">&#xe688;</i>
 			    			<i className="yo-ico">&#x3434;</i>
-			    			<i className="yo-ico">&#xe616;</i> > </i>
+			    			<i className="yo-ico">&#xe616;</i> &gt; </i>
 			    		</li>
 			    	</ul>
 			    	<ul>
 			    		<li>
-			    			<p><i className="yo-ico">&#xe610;</i>客服反馈</p>
-			    			<i> > </i>
+			    			<p><i className="yo-ico">&#xe610;</i><span>意见反馈</span></p>
+			    			<i> &gt; </i>
 			    		</li>
 			    		<li>
-			    			<p><i className="yo-ico">&#xe61b;</i>客服反馈</p>
-			    			<i> > </i>
+			    			<p><i className="yo-ico">&#xe61b;</i><span>联系客服</span></p>
+			    			<i> &gt; </i>
 			    		</li>
 			    	</ul>
 			    	<ul>
 			    		<li>
-			    			<p><i className="yo-ico">&#xe643;</i>客服反馈</p>
-			    			<i> > </i>
+			    			<p><i className="yo-ico">&#xe643;</i><span>积分商城</span></p>
+			    			<i> &gt; </i>
 			    		</li>
 			    	</ul>
-			    </section>
+			    </div>
 			</div>
 		)
 	}
