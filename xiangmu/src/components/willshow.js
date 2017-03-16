d91edcd7c37165a3a19a1aba0064d58ba72fd49b
hqp1988 <huangqingpengkeda@126.com>
2017-03-16 20:18:23 +0800

    tianjia scroll at moive

--- xiangmu/src/components/willshow.js
+++ xiangmu/src/components/willshow.js
@@ -1,8 +1,20 @@
 
 "use strict";
 
-
-
+ import React from "react"
+ 
+class WillShows extends React.Component{
+	
+	render(){
+		return(
+			<div>
+			     <div className="m-willshow">
+			        willshow
+			    </div>
+			</div>
+		)
+	}
+}
 
 
 
