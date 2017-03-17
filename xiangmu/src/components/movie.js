f2e5d26ecaa66a03cf04e4f7f8dd24f0cbbee15d
hqp1988 <huangqingpengkeda@126.com>
2017-03-17 19:49:16 +0800

    dff

--- xiangmu/src/components/movie.js
+++ xiangmu/src/components/movie.js
@@ -4,7 +4,7 @@ import React from "react"
 
 import {Link} from "react-router"
 
-import Scroller from "../../component_dev/scroller/src/index"
+import Scroller from "../../component_dev/scroller/src/"
 
 import WillShows from "./willshow"
 
@@ -12,28 +12,26 @@ class Movie extends React.Component{
 	render(){
 		return(
 			<div className="m-movie">
-			    <Scroller>
-				    <div>
-							<header>
-								<div>
-									<p>北京</p>
-									<i className="yo-ico">&#xe6a1;</i>
-								</div>
-								<div>
-									<span>电影</span>
-									<span>影院</span>
-								</div>
-								<button><i className="yo-ico">&#xe60a;</i></button>
-								<div>
-									<h3>正在热映</h3>
-									<h3>即将上映</h3>
-								</div>
-							</header>
-							<section>
-	                            <WillShows />
-							</section>
+			    
+				<header>
+					<div>
+						<p>北京</p>
+						<i className="yo-ico">&#xe6a1;</i>
 					</div>
-			    </Scroller>
+					<div>
+						<span>电影</span>
+						<span>影院</span>
+					</div>
+					<button><i className="yo-ico">&#xe60a;</i></button>
+					<div>
+						<h3>正在热映</h3>
+						<h3>即将上映</h3>
+					</div>
+				</header>
+				<section>
+                    <WillShows />
+				</section>
+				
 			</div>
 
 		)
--- xiangmu/src/components/willshow.js
+++ xiangmu/src/components/willshow.js
@@ -3,14 +3,30 @@
 
  import React from "react"
  
+ import Scroller from "../../component_dev/scroller/src/"
+ 
 class WillShows extends React.Component{
 	
 	render(){
 		return(
-			<div>
-			     <div className="m-willshow">
-			        willshow
-			    </div>
+			<div  className="m-willshow">
+			    <Scroller>
+			         <div className="months">
+			            <span>3</span>
+		                <span>4</span>
+		                <span>5</span>
+		                <span>6</span>
+		                <span>7</span>
+			        </div>
+			        <div className="movies">
+			            <div>
+			                <p>3月17日星期五</p>
+			                 <div>
+			                 
+			                 </div>
+			            </div>
+			        </div>
+			    </Scroller>
 			</div>
 		)
 	}
